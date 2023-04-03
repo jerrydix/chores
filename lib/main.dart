9b092256f3e4e33c7a4afd083306533cda04d2a2
J3rry <jerrygamerhd@gmail.com>
2023-04-03 20:00:34 +0200

    added firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'navigationbar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -28,31 +34,44 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  ThemeData setThemeData(ColorScheme? scheme, Brightness brightness) {
+    if (scheme != null) {
+      return ThemeData(
+        useMaterial3: true,
+        brightness: brightness,
+        colorScheme: scheme,
+      );
+    }
+    return ThemeData(
+      useMaterial3: true,
+      brightness: brightness,
+      colorSchemeSeed: Colors.teal,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chores',
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-      ),
-      themeMode: _themeMode,
-      localizationsDelegates: const [
-        AppLocalizations.delegate, // Add this line
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('de'),
-        Locale('ru'),
-      ],
-      home: const NavBar(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return MaterialApp(
+          title: 'Chores',
+          theme: setThemeData(lightDynamic, Brightness.light),
+          darkTheme: setThemeData(darkDynamic, Brightness.dark),
+          themeMode: _themeMode,
+          localizationsDelegates: const [
+            AppLocalizations.delegate, // Add this line
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'),
+            Locale('de'),
+            Locale('ru'),
+          ],
+          home: const NavBar(),
+        );
+      }
     );
   }
 }
