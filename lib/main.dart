13ebf51f7b0f2516abce95977b3ec1ac829ac03f
J3rry <jerrygamerhd@gmail.com>
2023-09-12 15:36:53 +0300

    updated build.gradle

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'firebase_options.dart';
 import 'utils/userprefs.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
