5cdb71c28821d152e073fe09a6c30736ee00de4d
J3rry <jerrygamerhd@gmail.com>
2024-08-21 13:55:40 +0200

    leave WG and logout from wg selection

--- lib/wg_selection.dart
+++ lib/wg_selection.dart
@@ -1,7 +1,12 @@
 
+import 'package:chores/user_auth/authentication_provider.dart';
+import 'package:chores/user_auth/pages/login.dart';
 import 'package:chores/widgets/wg_listtile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:chores/settings.dart';
 
 class WGSelection extends StatefulWidget {
   final String username;
@@ -24,6 +29,34 @@ class _WGSelectionState extends State<WGSelection> {
   Widget build(BuildContext context) {
     var db = FirebaseFirestore.instance;
 
+    Future openLogoutDialog() => showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              AppLocalizations.of(context)!.logout_t,
+              style: const TextStyle(fontSize: 20),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(AppLocalizations.of(context)!.cancel)),
+              TextButton(
+                  onPressed: () {
+                    AuthenticationProvider(FirebaseAuth.instance).signOut();
+                    Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ), (route) => false
+                    );
+                  },
+                  child: Text(AppLocalizations.of(context)!.confirm)),
+            ],
+          );
+        });
+
     return StreamBuilder<QuerySnapshot>(
       stream: db.collection("wgs").snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -35,7 +68,7 @@ class _WGSelectionState extends State<WGSelection> {
           return SelectionArea(
             child: Scaffold(
                 appBar: AppBar(
-                  title: const Text("Select WG"),
+                  title: Text(AppLocalizations.of(context)!.select_wg),
                 ),
                 body: const Center(child: CircularProgressIndicator())));
         }
@@ -44,6 +77,14 @@ class _WGSelectionState extends State<WGSelection> {
           child: Scaffold(
             appBar: AppBar(
               title: const Text("Select WG"),
+              actions: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: ElevatedButton(onPressed: () {
+                    openLogoutDialog();
+                  }, child: Text(AppLocalizations.of(context)!.logout)),
+                ),
+              ],
             ),
             body: ListView.separated(
               //padding: const EdgeInsets.all(8),
