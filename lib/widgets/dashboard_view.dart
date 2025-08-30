d75f29a85e05f4cbdeeaf6b7207c38f66a209ba8
J3rry <jerrygamerhd@gmail.com>
2025-08-31 00:59:19 +0200

    chore: update SDK versions and refactor localization imports

--- lib/widgets/dashboard_view.dart
+++ lib/widgets/dashboard_view.dart
@@ -4,8 +4,8 @@ import 'package:chores/member_manager.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../l10n/app_localizations.dart';
 import '../widgets/secondary_card.dart';
 
 import 'navigationbar.dart' as navBar;
