abd3e247605b4e93b356ff895ca7f3b004589e9a
Jeremy Dix <jeremy.dix@tum.de>
2026-05-16 23:39:10 +0200

    Add chinese localization, enable greek localization
    Signed-off-by: Jeremy Dix <jeremy.dix@tum.de>

---
+++ lib/l10n/app_localizations_zh.dart
@@ -0,0 +1,286 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Chinese (`zh`).
+class AppLocalizationsZh extends AppLocalizations {
+  AppLocalizationsZh([String locale = 'zh']) : super(locale);
+
+  @override
+  String get settings => '设置';
+
+  @override
+  String get dashboard => '任务';
+
+  @override
+  String get overview => '总览';
+
+  @override
+  String get about => '关于';
+
+  @override
+  String get theme => '主题';
+
+  @override
+  String get language => '语言';
+
+  @override
+  String get notEnable => '通知';
+
+  @override
+  String get notInterval => '通知计划';
+
+  @override
+  String get sys => '系统默认';
+
+  @override
+  String get dark => '深色';
+
+  @override
+  String get light => '浅色';
+
+  @override
+  String get monday => '每周一次（周一）';
+
+  @override
+  String get sunday => '每周一次（周六）';
+
+  @override
+  String get twice => '每周两次';
+
+  @override
+  String get every => '每天';
+
+  @override
+  String get info => '信息';
+
+  @override
+  String get options => '选项';
+
+  @override
+  String get confirm => '确认';
+
+  @override
+  String get cancel => '取消';
+
+  @override
+  String get friday => '每周一次（周三）';
+
+  @override
+  String get en => 'English';
+
+  @override
+  String get de => 'Deutsch';
+
+  @override
+  String get ru => 'Русский';
+
+  @override
+  String get el => 'Ελληνικά';
+
+  @override
+  String get zh => '中文（简体）';
+
+  @override
+  String get or => '或继续使用';
+
+  @override
+  String get leave_wg => '退出合租公寓';
+
+  @override
+  String get leave_wg_t => '确定要退出合租公寓吗？';
+
+  @override
+  String get wg_settings => '合租公寓设置';
+
+  @override
+  String get select_wg => '选择合租公寓';
+
+  @override
+  String get forgot_pw => '忘记密码？';
+
+  @override
+  String get reset_pw => '重置密码';
+
+  @override
+  String get reset_pw_t => '输入您的电子邮件地址以接收密码重置链接。';
+
+  @override
+  String get reset_pw_b => '发送重置链接';
+
+  @override
+  String get new_wg => '创建新合租公寓';
+
+  @override
+  String get wg_name => '名称';
+
+  @override
+  String get wg_use_template => '使用家务模板';
+
+  @override
+  String get wg_exists => '该名称已被使用，请选择其他名称。';
+
+  @override
+  String get garbage => '垃圾负责人';
+
+  @override
+  String get g100plastic => '塑料垃圾';
+
+  @override
+  String get g101residual => '其他垃圾';
+
+  @override
+  String get g102organic => '厨余垃圾';
+
+  @override
+  String get g103paper => '纸类垃圾';
+
+  @override
+  String get g104glass => '玻璃垃圾';
+
+  @override
+  String get g105bottles => '归还押金瓶';
+
+  @override
+  String get bathroom => '浴室负责人';
+
+  @override
+  String get b206mirrors => '擦镜子';
+
+  @override
+  String get b207sink => '清洁洗手台';
+
+  @override
+  String get b208bath => '清洁浴缸';
+
+  @override
+  String get b209toilet => '清洁马桶';
+
+  @override
+  String get b210dust => '擦拭架子和各表面的灰尘';
+
+  @override
+  String get b211carpet => '更换地垫';
+
+  @override
+  String get b212towel => '更换毛巾';
+
+  @override
+  String get b213b_waste => '清理浴室垃圾';
+
+  @override
+  String get kitchen => '厨房负责人';
+
+  @override
+  String get b314surfaces => '清洁（玻璃）台面（含桌子）';
+
+  @override
+  String get b315stove => '清洁灶台和烤箱';
+
+  @override
+  String get b316fridge => '清空并清洁冰箱';
+
+  @override
+  String get b317cloths => '清洗抹布、毛巾和地垫';
+
+  @override
+  String get vacuum => '吸尘负责人';
+
+  @override
+  String get b418v_kitchen => '吸尘——厨房';
+
+  @override
+  String get b419v_bath => '吸尘——浴室';
+
+  @override
+  String get b420v_living => '吸尘——客厅';
+
+  @override
+  String get b421v_stairs => '吸尘——楼梯';
+
+  @override
+  String get b422v_corridor => '吸尘——走廊';
+
+  @override
+  String get b423wipe => '拖地';
+
+  @override
+  String get logout => '退出登录';
+
+  @override
+  String get logout_t => '退出 Chores？';
+
+  @override
+  String get cw => '第';
+
+  @override
+  String get n_title => '提醒';
+
+  @override
+  String get n_text => '完成本周的家务任务。';
+
+  @override
+  String get active => '可参与工作';
+
+  @override
+  String get noPrimaryRoles => '您当前不参与家务分配。';
+
+  @override
+  String get noPrimaryRoles2 => '暂未分配任何家务。';
+
+  @override
+  String get uname => '用户名';
+
+  @override
+  String get email => '电子邮件';
+
+  @override
+  String get pw => '密码';
+
+  @override
+  String get login => '登录 Chores';
+
+  @override
+  String get login_b => '登录';
+
+  @override
+  String get signup => '注册 Chores';
+
+  @override
+  String get signup_b => '注册';
+
+  @override
+  String get noAccount => '还没有账号？';
+
+  @override
+  String get haveAccount => '已有账号？';
+
+  @override
+  String get finAllTasks => '所有任务已完成';
+
+  @override
+  String get role_task_settings => '角色与任务设置';
+
+  @override
+  String get algo_settings => '角色分配';
+
+  @override
+  String get algo_rotating => '每周轮换';
+
+  @override
+  String get algo_rotating_desc => '角色每周轮换，确保人人参与';
+
+  @override
+  String get algo_fixed => '固定不变';
+
+  @override
+  String get algo_fixed_desc => '每周将相同角色分配给相同人员';
+
+  @override
+  String get algo_random => '随机（公平）';
+
+  @override
+  String get algo_random_desc => '每周随机分配，长期保持平衡';
+}
